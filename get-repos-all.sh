0d28ccc92f1c9ba08f909977b626f49c55fac1d0
Lucas Russo <lerwys@gmail.com>
2018-04-19 11:29:10 -0300

    get-repos-all.sh: add special clone case for CA gateway
    As it lives in another github organization, we must
    treat it differently.

--- get-repos-all.sh
+++ get-repos-all.sh
@@ -14,7 +14,12 @@ for repo in "${GITHUB_REPOS[@]}"; do
         # Remove prefix if any
         repo_noprefix=$(echo "${repo}" | sed -e "s#^${GITHUB_REPOS_PREFIX}##g")
 
-        git clone --recursive ${GITHUB_BASE_URL}/${GITHUB_ORG}/${repo_noprefix}
+        # Special place for CA gateway
+        if [ ! -z "$(echo ${repo} | grep "docker-ca-gateway-composed-deploy")" ]; then
+            git clone --recursive ${GITHUB_CA_GATEWAY_BASE_URL}/${GITHUB_CA_GATEWAY_ORG}/${repo_noprefix}
+        else
+            git clone --recursive ${GITHUB_BASE_URL}/${GITHUB_ORG}/${repo_noprefix}
+        fi
     else
        bash -c "\
            cd ${repo} && \
