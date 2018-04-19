0967769d7f2d525d42225110bbd0263aeb66401c
Lucas Russo <lerwys@gmail.com>
2018-04-19 11:28:13 -0300

    get-repos-all.sh: remove any possible prefix from repo name
    This prefix is only used for existing repos.
    When we want to clone the repo, we must remove them.

--- get-repos-all.sh
+++ get-repos-all.sh
@@ -11,7 +11,10 @@ CURDIR="$(dirname $(readlink -f $0))"
 # CLone all repos
 for repo in "${GITHUB_REPOS[@]}"; do
     if [ -z "$(ls -A ${repo})" ]; then
-        git clone --recursive ${GITHUB_BASE_URL}/${GITHUB_ORG}/${repo}
+        # Remove prefix if any
+        repo_noprefix=$(echo "${repo}" | sed -e "s#^${GITHUB_REPOS_PREFIX}##g")
+
+        git clone --recursive ${GITHUB_BASE_URL}/${GITHUB_ORG}/${repo_noprefix}
     else
        bash -c "\
            cd ${repo} && \
